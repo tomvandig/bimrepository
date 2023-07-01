979546cf6f2bbd227e291c63028095f28d031266
Tom van Diggelen <49716596+tomvandig@users.noreply.github.com>
2023-07-01 11:50:15 +0200

    Gen schema to fb

--- convert.js
+++ convert.js
@@ -32,8 +32,77 @@ function genProp(name, prop)
     {
         return `${name}: ${genArray(prop)} = [];`;
     }
+    if (prop.type === "number")
+    {
+        return `${name}: number = 0`;
+    }
 
     return `<unknown type ${prop.type}>`;
+}
+
+function propTypeToFBType(propType)
+{
+    if (propType === "array")
+    {
+        return "PropertyType.Array";
+    }
+    else if (propType === "number")
+    {
+        return "PropertyType.Number";
+    }
+    return "PropertyType.Unknown";
+}
+
+function genItems(propname, property)
+{
+    return `
+        ${propname}.items = new ItemsT();
+        ${propname}.items.type = ${propTypeToFBType(property.items.type)};
+    `;
+}
+
+function genPropertySchemaCode(name, property)
+{
+    let propname = `prop_${name}`;
+
+    return `
+        // property ${name}
+        {
+            let ${propname} = new propertyT();
+
+            ${propname}.name = "${name}";
+            ${propname}.type = ${propTypeToFBType(property.type)};
+            ${property.type === "array" ? genItems(propname, property) : ""}
+
+            schemaObj.schemaShape.properties.push(${propname});
+        }
+    `;
+}
+
+function genSchemaShapeCode(shape)
+{
+    return `
+        schemaObj.schemaShape = new shapeT();
+
+        ${Object.keys(shape).map(pname => genPropertySchemaCode(pname, shape[pname])).join("\n")}
+    `;
+}
+
+function genSchemaExportCode(schema)
+{
+    return `
+    static exportDefinitionToArray(builder: flatbuffers.Builder) {
+        let schemaObj = new SchemaT();
+        schemaObj.id = [${schema["$id"].map(e => `"${e}"`).join(",")}];
+        schemaObj.schemaversion = "${schema["$schemaversion"]}";
+        schemaObj.comment = "${schema["$comment"]}";
+        schemaObj.description = "${schema["description"]}";
+
+        ${genSchemaShapeCode(schema.shape)}
+
+        Schema.finishSchemaBuffer(builder, schemaObj.pack(builder));
+    }
+    `
 }
 
 function genClass(schema)
@@ -41,9 +110,11 @@ function genClass(schema)
     console.log(schema);
 
     let id = schema["$id"];
+    let comment = schema["$comment"];
 
     let name = id[id.length - 1];
     let code =  dedent`
+        ${comment ? `// ${comment}` : ""}
         export class ${name} {
             
             // properties
@@ -65,9 +136,7 @@ function genClass(schema)
                 return new ${name}();
             }
             
-            static exportDefinitionToArray(builder: flatbuffers.Builder) {
-
-            }
+            ${genSchemaExportCode(schema)}
             // end statics
         }
     `;
@@ -76,9 +145,8 @@ function genClass(schema)
     {
         code = namespace(id[i], code);
     }
-
     
-    var indented = indent.js(code, {tabString: '  '});
+    var indented = indent.js(code, {tabString: '    '});
 
     return indented;
 }
@@ -87,8 +155,15 @@ function convert(path)
 {
     let schema = JSON.parse(fs.readFileSync(path));
 
-    let fbimport = `import * as flatbuffers from 'flatbuffers';`;
-    let header = `\n/*\n${JSON.stringify(schema, null, 4)}\n*/\n\n${fbimport}\n`;
+    let fbimports = [
+        `import * as flatbuffers from 'flatbuffers';`,
+        `import { Schema, SchemaT } from '../bimrepo/schema';`,
+        `import { shapeT } from '../bimrepo/shape';`,
+        `import { propertyT } from '../bimrepo/property';`,
+        `import { ItemsT } from '../bimrepo/items';`,
+        `import { PropertyType } from '../bimrepo/property-type';`
+    ].join("\n");
+    let header = `\n/*\n${JSON.stringify(schema, null, 4)}\n*/\n\n${fbimports}\n`;
     let output = `${header}\n${genClass(schema)}`;
 
     let cliOutput = highlight(output, {ignoreIllegals: true});
