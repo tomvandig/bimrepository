5a37b3ff5c07a127674c3eca17466b1832e31cad
Tom van Diggelen <49716596+tomvandig@users.noreply.github.com>
2023-07-08 23:55:28 +0200

    Setup websocket along api express

--- api.ts
+++ api.ts
@@ -1,14 +1,28 @@
 import { ByteBuffer } from "flatbuffers";
 import { CommitProposal, CommitProposalT } from "./bimrepo";
 import { ServerLedger } from "./server_ledger";
-
+import * as http from "http";
+import { WebSocketServer } from 'ws';
 const express = require('express')
+
+const wss = new WebSocketServer({ noServer: true });
 const app = express()
 const port = 3000
 
 let ledger = new ServerLedger();
 
 app.use(express.raw());
+
+
+wss.on('connection', function connection(connection) {
+  connection.on('error', console.error);
+  
+  connection.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  connection.send('something');
+});
 
 function toArrayBuffer(buffer) {
     const arrayBuffer = new ArrayBuffer(buffer.length);
@@ -32,7 +46,22 @@ app.post('/commit', (req, res) => {
 
     res.send('POST request to the homepage')
 })
+
+let server = http.createServer(app);
+
+
+server.on('upgrade', function upgrade(request, socket, head) {
+  const { pathname } = new URL(request.url!);
   
-app.listen(port, () => {
+  if (pathname === '/ws') {
+      wss.handleUpgrade(request, socket, head, function done(ws) {
+        wss.emit('connection', ws, request);
+      });
+  } else {
+    socket.destroy();
+  }
+});
+
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
