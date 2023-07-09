21a3ceaa9eed6900acbe9e219d0740862b639814
Tom van Diggelen <49716596+tomvandig@users.noreply.github.com>
2023-07-09 12:50:49 +0200

    Add helper functions

---
+++ helper.ts
@@ -0,0 +1,47 @@
+import { ComponentDataT, ComponentDataType, ComponentT } from "./bimrepo";
+
+
+export function Expect(component: ComponentT, type: ComponentDataType)
+{
+    let data = component.data.shift();
+    if (data.type !== ComponentDataType.Number)
+    {
+        throw new Error(`Expected number but received ${data.type}`);
+    }
+    return data;
+}
+
+export function GetNumber(component: ComponentT)
+{
+    let data = Expect(component, ComponentDataType.Number);
+    return data.num;
+}
+
+export function GetArrayStart(component: ComponentT)
+{
+    let data = Expect(component, ComponentDataType.ArrayStart);
+    return data.arrayLength;
+}
+
+export function MakeArrayStart(length: number)
+{
+    let p = new ComponentDataT();
+    p.type = ComponentDataType.ArrayStart;
+    p.arrayLength = length;
+    return p;
+}
+
+export function MakeArrayEnd()
+{
+    let p = new ComponentDataT();
+    p.type = ComponentDataType.ArrayEnd;
+    return p;
+}
+
+export function MakeNumber(num: number)
+{
+    let p = new ComponentDataT();
+    p.type = ComponentDataType.Number;
+    p.num = num;
+    return p;
+}
